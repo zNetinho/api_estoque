c2864f6014d24939a7a4a889a6fe4007b0721cf0
Neto Flavio <netoflavio97@hotmail.com>
2023-10-28 20:58:06 -0300

    chore/Criando o modelo de categorias

---
+++ src/models/CategoriaModels.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const CategoriaSchema = new Schema({
+    id:{
+      type: required,
+      required: true
+    },
+    nome: {
+      type: String,
+      required: true
+    },
+    descricao: {
+      type: String,
+      required: true,
+    },
+    title_seo: {
+      type: String,
+      min: 5,
+      max: 60,
+    },
+    descricao_seo: {
+      type: String,
+    },
+    slug: {
+      type: String,
+      required: true
+    },
+    produtos: {
+      type: Array,
+      required: true,
+      default: []
+    }
+
+})
+
+module.exports = mongoose.model("Categoria", ProductsModel)
